0424a208c2f73f5fa4d6e8317a2f9d6405dca730
Phat <you@example.com>
2020-08-29 23:48:55 +0700

    fix bugs

--- index.js
+++ index.js
@@ -1,66 +1,8 @@
-const arrows = document.querySelector(".arrows")
-const up = document.getElementById("up");
-const down = document.getElementById("down");
-const left = document.getElementById("left");
-const right = document.getElementById("right");
-
-var hasTouchScreen = false;
-if ("maxTouchPoints" in navigator) { 
-    hasTouchScreen = navigator.maxTouchPoints > 0;
-} else if ("msMaxTouchPoints" in navigator) {
-    hasTouchScreen = navigator.msMaxTouchPoints > 0; 
-} else {
-    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
-    if (mQ && mQ.media === "(pointer:coarse)") {
-        hasTouchScreen = !!mQ.matches;
-    } else if ('orientation' in window) {
-        hasTouchScreen = true; // deprecated, but good fallback
-    } else {
-        // Only as a last resort, fall back to user agent sniffing
-        var UA = navigator.userAgent;
-        hasTouchScreen = (
-            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-        );
-    }
-}
-if (hasTouchScreen)
-    arrows.style.display= "flex";
-
-let direction;
-
-up.addEventListener("click", function () {
-    if (direction != 'DOWN') {
-        direction = 'UP';
-    }
-    snake.changeDirection(direction);
-});
-
-down.addEventListener("click", function () {
-    if (direction != 'UP') {
-        direction = 'DOWN';
-    }
-    snake.changeDirection(direction);
-});
-
-left.addEventListener("click", function () {
-    if (direction != 'RIGHT') {
-        direction = 'LEFT';
-    }
-    snake.changeDirection(direction);
-});
-
-right.addEventListener("click", function () {
-    if (direction != 'LEFT') {
-        direction = 'RIGHT';
-    }
-    snake.changeDirection(direction);
-});
-
-
+//declare snake and food
 let snake;
 let food;
 
+//setup the game
 (function setup() {
     //create snake and food
     snake = new Snake();
@@ -95,22 +37,3 @@ let food;
         snake.checkCollision();
     }, 150)
 })();
-
-
-
-//change snake direction
-window.addEventListener('keydown', (event) => {
-    const keyCode = event.keyCode;
-    if (keyCode === 37 && direction !== "RIGHT") {
-        direction = "LEFT";
-    } else if (keyCode === 38 && direction !== "DOWN") {
-        direction = "UP";
-    } else if (keyCode === 39 && direction !== "LEFT") {
-        direction = "RIGHT"
-    } else if (keyCode === 40 && direction !== "UP") {
-        direction = "DOWN";
-    }
-
-    snake.changeDirection(direction);
-
-});
